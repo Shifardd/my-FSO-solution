88c6d0f1689e806d66c5ee17129fd20aa3c5712a
jsnspllrd@gmail.com <220439987+Shifardd@users.noreply.github.com>
2026-03-06 06:59:49 +0800

    feat: add total number of collected feedback for each category

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,10 +5,43 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGood = () => setGood(good + 1)
+  const handleNeutral = () => setNeutral(neutral + 1)
+  const handleBad = () => setBad(bad + 1)
+
   return (
     <div>
-      Code here
+      <SubTitle title="give feedback" />
+      <Button onClick={handleGood} text="Good" />
+      <Button onClick={handleNeutral} text="Neutral" />
+      <Button onClick={handleBad} text="Bad" />
+      <SubTitle title="statistics" />
+      <TheValue name="good" value={good} />
+      <TheValue name="neutral" value={neutral} />
+      <TheValue name="bad" value={bad} />
     </div>
+  )
+}
+
+const SubTitle = ({title}) => {
+  return (
+    <h2>
+      {title}
+    </h2>
+  )
+}
+
+const Button = ({onClick, text}) => {
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+
+const TheValue = ({name, value}) => {
+  return (
+    <p>{name} {value}</p>
   )
 }
 
