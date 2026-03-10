9caabd8aa8f6e0b97aae16348a6c91722c6b3fec
jsnspllrd@gmail.com <220439987+Shifardd@users.noreply.github.com>
2026-03-10 17:08:49 +0800

    feat: add number input and its logic

--- part2/Phonebook/src/App.jsx
+++ part2/Phonebook/src/App.jsx
@@ -2,24 +2,30 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {name: 'Arto Hellas'}
+    {
+      name: 'Arto Hellas',
+      phone: '040-1234567'
+    }
   ])
   const [newName, setNewName] = useState('')
+  const [newPhone, setNewPhone] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
     let namesListed = persons.map(person => person.name)
     
-    let nameToBeAdded = {
-      name: newName
+    let personToBeAdded = {
+      name: newName,
+      phone: newPhone
     }
-    // console.log(nameToBeAdded.name);
+    // console.log(personToBeAdded.name);
     
-    if (namesListed.includes(nameToBeAdded.name)) {
+    if (namesListed.includes(personToBeAdded.name)) {
       alert(`${newName} is already added to phonebook`)
     } else {
-      setPersons(persons.concat(nameToBeAdded))
+      setPersons(persons.concat(personToBeAdded))
       setNewName('')
+      setNewPhone('')
     }
     // console.log('button clicked ',event.target);
   }
@@ -28,9 +34,13 @@ const App = () => {
     setNewName(event.target.value)
     // console.log(event.target.value);
   }
-  
-  
 
+  const handleNewPhone = (event) => {
+    setNewPhone(event.target.value)
+    // console.log(event.target.value);
+  }
+  
+  
   return (
     <div>
       <h2>Phonebook</h2>
@@ -39,12 +49,15 @@ const App = () => {
           name: <input value={newName} onChange={handleNewName} />
         </div>
         <div>
+          number: <input value={newPhone} onChange={handleNewPhone} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {
-        persons.map(person => <div key={person.name}>{person.name}</div>)
+        persons.map(person => <div key={person.name}>{person.name} {person.phone}</div>)
       }
 
     </div>
