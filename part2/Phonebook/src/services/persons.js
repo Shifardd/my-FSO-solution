5db326d8f37632114bd6c1870e6223fe7773605c
jsnspllrd@gmail.com <220439987+Shifardd@users.noreply.github.com>
2026-03-18 03:18:12 +0800

    feat: add update function variable that handle put request

--- part2/Phonebook/src/services/persons.js
+++ part2/Phonebook/src/services/persons.js
@@ -16,5 +16,10 @@ const remove = (id) => {
   return request.then(response => response.data)
 }
 
-export default { getAll , create , remove }
+const update = (id, changedPerson) => {
+  const request = axios.put(`${baseUrl}/${id}`, changedPerson)
+  return request.then(response => response.data)
+}
+
+export default { getAll , create , remove , update }
 
