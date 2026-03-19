b068ed6b2cf9e6a1bf66742ffd2d9264de480277
jsnspllrd@gmail.com <220439987+Shifardd@users.noreply.github.com>
2026-03-20 01:17:17 +0800

    feat: display the basic data, languages, and flag of country if there is one match in the query

--- part2/DataForCountries/src/components/List.jsx
+++ part2/DataForCountries/src/components/List.jsx
@@ -1,4 +1,8 @@
 const List = ({filtered}) => {
+  let flagStyle = {
+    fontSize: 200
+  }
+
   if(filtered.length > 10) {
     return (
       <div>
@@ -6,6 +10,23 @@ const List = ({filtered}) => {
       </div>
     )
   } 
+  else if (filtered.length == 1) {
+    let languages = Object.values(filtered[0].languages)
+
+    return (
+      <>
+        <h1>{filtered[0].name.common}</h1>
+        <div>Capital {filtered[0].capital}</div>
+        <div>Area {filtered[0].area}</div>
+        <h2>Languages</h2>
+        <ul> 
+          {languages.map(lang => <li>{lang}</li>)}
+        </ul>
+        <div style={flagStyle}>{filtered[0].flag}</div>
+      </>
+      
+    )
+  }
 
   return (
     filtered.map(country => <div>{country.name.common}</div>)
