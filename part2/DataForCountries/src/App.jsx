f7ae489a7b11d770e0364996d6a2093985273283
jsnspllrd@gmail.com <220439987+Shifardd@users.noreply.github.com>
2026-03-23 11:49:10 +0800

    refactor: call the all countries then update the country based on the input matching, update the logic of handleInput function

--- part2/DataForCountries/src/App.jsx
+++ part2/DataForCountries/src/App.jsx
@@ -1,46 +1,81 @@
 import { useEffect, useState } from "react"
 import List from "./components/List"
 import countriesService from './services/countries'
+import axios from "axios"
 
 const App = () => {
   const [value, setValue] = useState('')
-  const [countries, setCountries] = useState(null)
+  const [countryData, setCountryData] = useState(null)
+  const [country, setCountry] = useState(null)
+  const [listOfCountries, setListOfCountries] = useState([]) 
+
+  useEffect(() => {
+    axios.get('https://studies.cs.helsinki.fi/restcountries/api/all')
+    .then(response => {
+      let countries = response.data.map(country => country.name.common)
+      setListOfCountries(countries)
+    })
+  }, [])  // list of countries name
 
   useEffect(()=> {
-    if(!countries) {
-      countriesService.getAll()
-      .then(countries => setCountries(countries))
+    if(country) {
+      axios.get(`https://studies.cs.helsinki.fi/restcountries/api/name/${country}`)
+      .then(response => {
+        let dataObject = {
+          name: response.data.name.common,
+          capital: response.data.capital[0],
+          area: response.data.area,
+          languages: response.data.languages,
+          flags: response.data.flags
+        }
+        setCountryData(dataObject)
+      })
     }
-  },[countries])
+  },[country])  // For country data
 
-  if(!countries) {
-    return null
+const handleInput = (event) => {
+  const newValue = event.target.value
+  setValue(newValue)
+
+  const newFilter = listOfCountries.filter(c => {
+    return c.toLowerCase().includes(newValue.toLowerCase())
+  }) 
+
+  if (newFilter.length == 1) {
+    let selected = newFilter[0]
+    if(selected != country) {
+      setCountry(selected)
+    }
+  } else {
+    setCountryData(null)
+    setCountry(null)
   }
+}
 
-  let handleInput = (event) => {
-    let newValue = event.target.value
-    setValue(newValue)
-  }
 
-  let filtered = countries.filter(c => c.name.common.toLowerCase().includes(value.toLowerCase()))  
+
+  let filtered = listOfCountries.filter(c => {
+    return c.toLowerCase().includes(value.toLowerCase())
+  })   // filter name of country
+  
 
   let showIt = (country) => {
-    setValue(country.name.common);    
+    setValue(country);
+    setCountry(country);    
   }
-
 
   return (
     <div>
       find countries <input value={value} onChange={handleInput} />
       <div>
         {
-          <List filtered={filtered} onClick={showIt} />
+          <List filtered={filtered} onClick={showIt} data={countryData} />
         }
       </div>
     </div>
-  )
+  )  
 }
 
 
 
-export default App 
+export default App
