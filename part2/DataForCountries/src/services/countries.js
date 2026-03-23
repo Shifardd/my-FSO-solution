41a36cea12525f0924693697c6f6dd3a3861dddd
jsnspllrd@gmail.com <220439987+Shifardd@users.noreply.github.com>
2026-03-23 11:55:29 +0800

    feat: update urls and create variable function for getting all countries and specific countries

--- part2/DataForCountries/src/services/countries.js
+++ part2/DataForCountries/src/services/countries.js
@@ -1,10 +1,16 @@
 import axios from "axios";
 
-const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api/all'
+const allURL = 'https://studies.cs.helsinki.fi/restcountries/api/all'
+const specificURL = 'https://studies.cs.helsinki.fi/restcountries/api/name/'
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
+  const request = axios.get(allURL)
   return request.then(response => response.data)
 }
 
-export default {getAll}
+const getSpecific = (name) => {
+  const request = axios.get(`${specificURL}/${name}`)
+  return request.then(response => response.data)
+}
+
+export default {getAll , getSpecific}
