245ffb200866a464dced79b9dbce79b181ef7df9
jsnspllrd@gmail.com <220439987+Shifardd@users.noreply.github.com>
2026-03-25 15:18:22 +0800

    feat: add a delete functionality to a single phonebook entry using HTTP Delete

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 
 const app = express()
 
-const data = [
+let data = [
     { 
       "id": "1",
       "name": "Arto Hellas", 
@@ -48,7 +48,12 @@ app.get('/api/persons/:id', (req, res) => {
   } else {
     res.status(404).end()
   }
-  
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+  data = data.filter(p => p.id !== id)
+  res.status(204).end()
 })
 
 const PORT = 3001
