d5c8ce0a59100a4d63ed96414a2ba1a944877782
jsnspllrd@gmail.com <220439987+Shifardd@users.noreply.github.com>
2026-09-05 20:15:50 +0800

    feat: add message UIcomponent

---
+++ part5/bloglist-frontend/src/components/Message.jsx
@@ -0,0 +1,13 @@
+const Message = ({message, color}) => {
+  const styleObject = `message ${color}`
+  if (message === null) {
+    return null
+  }
+  return (
+    <div className={styleObject}>
+      {message}
+    </div>
+  )
+}
+
+export default Message
